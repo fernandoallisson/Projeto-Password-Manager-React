37547fe1fcfe85ee6113733ac963246a5a6adb76
fernandoallisson <fernando.alisson@ufpe.br>
2023-06-06 19:54:16 -0300

    Adicionando funções para salvar no estado o que o user digitar nos inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { useState } from 'react';
 import './App.css';
 import Form from './components/Form';
 
 function App() {
+  const [cadastrar, setCadastrar] = useState(false);
+  const handleCadastrar = () => (cadastrar ? setCadastrar(false) : setCadastrar(true));
+
   return (
     <div>
       <h1>Gerenciador de senhas</h1>
-      <Form />
+      <button onClick={ handleCadastrar }>Cadastrar nova senha</button>
+      {cadastrar && <Form />}
     </div>
   );
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,25 +1,48 @@
+import React, { useState } from 'react';
+
 // type FormProps = {
 //   name = string,
 // }
 
 function Form() {
+  const [data, setData] = useState({
+    name: '',
+    login: '',
+    password: '',
+    url: '',
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => { // Qualquer mudança nos inputs eles alteram o estado.
+    const { id, value } = e.target;
+    setData({
+      ...data,
+      [id]: value,
+    });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // Depois que salvar vou adicionar aqui as informações para serem salvas
+  };
+  // ---------------------------------------------------------------------------------------
+
   return (
-    <form>
+    <form onSubmit={ handleSubmit }>
       <label htmlFor="name">
         Nome do serviço
-        <input type="text" name="name" id="name" />
+        <input type="text" name="name" id="name" onChange={ handleChange } />
       </label>
       <label htmlFor="login">
         Login
-        <input type="text" name="login" id="login" />
+        <input type="text" name="login" id="login" onChange={ handleChange } />
       </label>
       <label htmlFor="password">
         Senha
-        <input type="password" name="password" id="password" />
+        <input type="password" name="password" id="password" onChange={ handleChange } />
       </label>
       <label htmlFor="url">
         url
-        <input type="text" name="url" id="url" />
+        <input type="text" name="url" id="url" onChange={ handleChange } />
       </label>
       <button>Cadastrar</button>
       <button>Cancelar</button>
